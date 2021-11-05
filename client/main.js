086d8a5827f57aa1ad39f9d6c5e443614f8410dc
Tanner Stufflebeam <tannerstufflebeam@gmail.com>
2021-11-05 09:18:40 -0600

    initialize

---
+++ client/main.js
@@ -0,0 +1,7 @@
+document.getElementById("complimentButton").onclick = function () {
+    axios.get("http://localhost:4000/api/compliment/")
+        .then(function (response) {
+          const data = response.data;
+          alert(data);
+        });
+  };
